ab87825636da2da15f3d5234f4fe1edc3be715d2
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-03-13 23:42:04 +0100

    rgw: expose the merged attrs outside in ATTRSMOD_MERGE mode.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -3407,6 +3407,7 @@ static void set_copy_attrs(map<string, bufferlist>& src_attrs,
     for (map<string, bufferlist>::iterator it = attrs.begin(); it != attrs.end(); ++it) {
       src_attrs[it->first] = it->second;
     }
+    attrs = src_attrs;
     break;
   }
 }
