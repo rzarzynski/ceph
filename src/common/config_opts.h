4abe0b6e8f3fe6dd27c3e5def05c5a39d4910815
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-05-21 17:19:38 +0200

    rgw: add garbage collector daemon for expired objects.
    Fixes: #4099
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1054,6 +1054,7 @@ OPTION(rgw_multipart_min_part_size, OPT_INT, 5 * 1024 * 1024) // min size for ea
 
 OPTION(rgw_olh_pending_timeout_sec, OPT_INT, 3600) // time until we retire a pending olh change
 
+OPTION(rgw_objexp_gc_interval, OPT_U32, 60 * 10) // maximum time between round of expired objects garbage collecting
 OPTION(rgw_objexp_time_step_exp, OPT_U32, 12) // exponent value (2 is the base) for rounding the timestamps
 OPTION(rgw_objexp_hints_num_shards, OPT_U32, 127) // maximum number of parts in which the hint index is stored in
 
