71cfb4abb71ed02723454ee81eeb9552f71e2616
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-05-25 17:15:54 +0200

    rgw: make the rgw-object-expirer's options more human readable.
    Fixes: #4096
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1055,7 +1055,7 @@ OPTION(rgw_multipart_min_part_size, OPT_INT, 5 * 1024 * 1024) // min size for ea
 OPTION(rgw_olh_pending_timeout_sec, OPT_INT, 3600) // time until we retire a pending olh change
 
 OPTION(rgw_objexp_gc_interval, OPT_U32, 60 * 10) // maximum time between round of expired objects garbage collecting
-OPTION(rgw_objexp_time_step_exp, OPT_U32, 12) // exponent value (2 is the base) for rounding the timestamps
+OPTION(rgw_objexp_time_step, OPT_U32, 4096) // number of seconds for rounding the timestamps
 OPTION(rgw_objexp_hints_num_shards, OPT_U32, 127) // maximum number of parts in which the hint index is stored in
 
 OPTION(mutex_perf_counter, OPT_BOOL, false) // enable/disable mutex perf counter
