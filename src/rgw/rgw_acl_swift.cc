42e6caeb6667c6d33e89b8d5e5e328dc4b422029
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-01-14 18:20:14 +0100

    rgw: add support for Swift account ACL's deserialization from JSON.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_acl_swift.cc
+++ src/rgw/rgw_acl_swift.cc
@@ -138,6 +138,77 @@ void RGWAccessControlPolicy_SWIFT::to_str(string& read, string& write)
   }
 }
 
+void RGWAccessControlPolicy_SWIFTAcct::add_grants(RGWRados * const store,
+                                                  const list<string>& uids,
+                                                  const int perm)
+{
+  for (const auto uid : uids) {
+    ACLGrant grant;
+    RGWUserInfo grant_user;
+
+    if (uid_is_public(uid)) {
+      grant.set_group(ACL_GROUP_ALL_USERS, perm);
+      acl.add_grant(&grant);
+    } else  {
+      rgw_user user(uid);
+
+      if (rgw_get_user_info_by_uid(store, user, grant_user) < 0) {
+        ldout(cct, 10) << "grant user does not exist:" << uid << dendl;
+        /* skipping silently */
+      } else {
+        grant.set_canon(user, grant_user.display_name, perm);
+        acl.add_grant(&grant);
+      }
+    }
+  }
+}
+
+bool RGWAccessControlPolicy_SWIFTAcct::create(RGWRados * const store,
+                                              const rgw_user& id,
+                                              const std::string& name,
+                                              const std::string& acl_str)
+{
+  acl.create_default(id, name);
+  owner.set_id(id);
+  owner.set_name(name);
+
+  JSONParser parser;
+
+  if (!parser.parse(acl_str.c_str(), acl_str.length())) {
+    ldout(cct, 0) << "ERROR: JSONParser::parse returned error=" << dendl;
+    return false;
+  }
+
+  JSONObjIter iter = parser.find_first("admin");
+  if (!iter.end() && (*iter)->is_array()) {
+    list<string> admin;
+    decode_json_obj(admin, *iter);
+    ldout(cct, 0) << "admins: " << admin << dendl;
+
+    add_grants(store, admin, SWIFT_PERM_ADMIN);
+  }
+
+  iter = parser.find_first("read-write");
+  if (!iter.end() && (*iter)->is_array()) {
+    list<string> readwrite;
+    decode_json_obj(readwrite, *iter);
+    ldout(cct, 0) << "read-write: " << readwrite << dendl;
+
+    add_grants(store, readwrite, SWIFT_PERM_RWRT);
+  }
+
+  iter = parser.find_first("read-only");
+  if (!iter.end() && (*iter)->is_array()) {
+    list<string> readonly;
+    decode_json_obj(readonly, *iter);
+    ldout(cct, 0) << "read-only: " << readonly << dendl;
+
+    add_grants(store, readonly, SWIFT_PERM_READ);
+  }
+
+  return true;
+}
+
 void RGWAccessControlPolicy_SWIFTAcct::to_str(std::string& acl_str) const
 {
   list<string> admin;
