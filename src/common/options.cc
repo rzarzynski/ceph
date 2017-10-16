6769b8314e83b1b374dff06b1e8ea60df26d9003
Radoslaw Zarzynski <rzarzyns@redhat.com>
2017-10-16 18:12:36 +0200

    os/bluestore: enable huge pages globally in RocksDB.
    This might be something we don't really want to do
    because of x86 MMU's limitations. Basically, it has
    less resources for huge pages in comparison to 4 kB
    pages. For instance, STLB doesn't have any entries
    for them while DTLB offers only 32 entries for 2 MB/
    4 MB and 4 for 1 GB.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -3479,7 +3479,7 @@ std::vector<Option> get_global_options() {
     .set_description("Max transactions with deferred writes that can accumulate before we force flush deferred writes"),
 
     Option("bluestore_rocksdb_options", Option::TYPE_STR, Option::LEVEL_ADVANCED)
-    .set_default("compression=kNoCompression,max_write_buffer_number=4,min_write_buffer_number_to_merge=1,recycle_log_file_num=4,write_buffer_size=268435456,writable_file_max_buffer_size=0,compaction_readahead_size=2097152")
+    .set_default("memtable_huge_page_size=2097152,compression=kNoCompression,max_write_buffer_number=4,min_write_buffer_number_to_merge=1,recycle_log_file_num=4,write_buffer_size=268435456,writable_file_max_buffer_size=0,compaction_readahead_size=2097152")
     .set_description("Rocksdb options"),
 
     Option("bluestore_rocksdb_cf", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
