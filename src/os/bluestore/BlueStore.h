286ece77adc218415255629c3894e89324dcc608
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-01-03 18:32:00 +0100

    os/bluestore: implement ReadTransaction of ObjectStore.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1780,10 +1780,16 @@ public:
       read_ctx_t(async_read_params_t params)
         : params(std::move(params)) {
       }
+
+      read_ctx_t(async_read_params_t&& params,
+                 cache_response_t&& cache_response)
+        : params(std::move(params)),
+          cache_response(std::move(cache_response)) {
+      }
     };
 
     std::vector<read_ctx_t> read_ctx_batch;
-    Context* const on_all_complete;
+    Context* on_all_complete;
     IOContext ioc;
 
     AioReadBatch(CephContext* const cct,
@@ -1792,8 +1798,20 @@ public:
         ioc(cct, this, true) { // allow EIO
     }
 
+    AioReadBatch(CephContext* const cct)
+      : on_all_complete(nullptr),
+        ioc(cct, this, true) { // allow EIO
+    }
+
     read_ctx_t& create_read_ctx(async_read_params_t params) {
       read_ctx_batch.push_back(std::move(params));
+      return read_ctx_batch.back();
+    }
+
+    read_ctx_t& queue_read_ctx(cache_response_t&& cache_response,
+                               async_read_params_t&& params) {
+      read_ctx_batch.emplace_back(std::move(params),
+                                  std::move(cache_response));
       return read_ctx_batch.back();
     }
 
@@ -2405,6 +2423,62 @@ public:
     size_t len,
     bufferlist& bl,
     uint32_t op_flags = 0);
+
+  class BlueReadTrans : public ObjectStore::ReadTransaction {
+    BlueStore* const store;
+    CollectionHandle c;
+    OnodeRef o;
+    AioReadBatch* aio = nullptr;
+
+    int _do_read(
+      uint64_t offset,
+      uint64_t length,
+      uint32_t flags,
+      ceph::bufferlist& destbl,
+      Context* on_complete);
+
+  public:
+    BlueReadTrans(BlueStore* const store,
+                  CollectionHandle&& c,
+                  OnodeRef&& o)
+    : store(store),
+      c(std::move(c)),
+      o(std::move(o)) {
+    }
+
+    int read(
+      uint64_t offset,
+      uint64_t length,
+      uint32_t flags,
+      ceph::bufferlist& destbl,
+      Context* on_complete) override;
+
+    bool empty() const override {
+      return !aio || aio->read_ctx_batch.empty();
+    }
+
+    int apply(Context* on_complete) override;
+  };
+
+  std::unique_ptr<ReadTransaction> create_read_transaction(
+    const coll_t& cid,
+    const ghobject_t& oid
+  ) override {
+    CollectionHandle c_ = _get_collection(cid);
+    OnodeRef o;
+
+    Collection *c = static_cast<Collection *>(c_.get());
+
+    if (c->exists) {
+      RWLock::RLocker l(c->lock);
+      PerfGuard(logger, l_bluestore_read_onode_meta_lat);
+
+      o = c->get_onode(oid, false);
+    }
+
+    // ENOENTs will be handled later in BlueReadTrans::read().
+    return std::make_unique<BlueReadTrans>(this, std::move(c), std::move(o));
+  }
 
   bool has_async_read() const override {
     return true;
