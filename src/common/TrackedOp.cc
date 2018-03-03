b3a7e8fff8052a6e4c9707ad9f1af1f882d2d0c7
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-03-03 04:45:15 +0100

    common: optimize out divisions in OpTracker.
    Before:
    ------
    
    ```
    Samples: 116K of event 'cycles:pp', Event count (approx.): 21847258285
      Children      Self  Command          Shared Object         Symbol            ◆
    -   52,44%     0,00%  tp_osd_tp        [unknown]             [k] 0xffffffffffff▒
       - 0xffffffffffffffff                                                        ▒
          - 51,22% __clone                                                         ▒
               start_thread                                                        ▒
               ShardedThreadPool::WorkThreadSharded::entry                         ▒
             - ShardedThreadPool::shardedthreadpool_worker                         ▒
                - 50,95% OSD::ShardedOpWQ::_process                                ▒
                   + 43,49% PGOpItem::run                                          ▒
                   + 1,77% Cond::Wait                                              ▒
                   + 1,53% PG::lock                                                ▒
                   - 1,28% PGOpItem::~PGOpItem                                     ▒
                      - TrackedOp::put                                             ▒
                         - 1,00% OpTracker::unregister_inflight_op                 ▒
                              0,58% OpRequest::_unregistered                       ▒
    ```
    OpTracker::unregister_inflight_op  /work/ceph-1/build/bin/ceph-osd
    Percent│       mov    %rsi,%rbx
      0,56 │       mov    %rdi,%rbp
           │     _ZN5Mutex6LockerC4ERS_():
           │       class Locker {
           │         Mutex &mutex;
           │
           │       public:
           │         explicit Locker(Mutex& m) : mutex(m) {
           │           mutex.Lock();
           │       xor    %esi,%esi
           │     _ZN9OpTracker22unregister_inflight_opEP9TrackedOp():
     34,83 │       divq   0x8(%rdi)
           │     _ZN4ceph11tiny_vectorIN9OpTracker19ShardedTrackingDataELm32EEixEm()
           │           delete[] data;
           │         }
           │       }
           │
    ```
    
    After
    -----
    
    ```
    Samples: 92K of event 'cycles:pp', Event count (approx.): 17708492121
      Children      Self  Command          Shared Object         Symbol            ◆
    -   52,49%     0,00%  tp_osd_tp        [unknown]             [k] 0xffffffffffff▒
       - 0xffffffffffffffff                                                        ▒
          - 51,26% __clone                                                         ▒
               start_thread                                                        ▒
               ShardedThreadPool::WorkThreadSharded::entry                         ▒
             - ShardedThreadPool::shardedthreadpool_worker                         ▒
                - 50,99% OSD::ShardedOpWQ::_process                                ▒
                   + 44,01% PGOpItem::run                                          ▒
                   + 1,58% Cond::Wait                                              ▒
                   + 1,49% PG::lock                                                ▒
                   - 1,16% PGOpItem::~PGOpItem                                     ▒
                      - TrackedOp::put                                             ▒
                         - 0,86% OpTracker::unregister_inflight_op                 ▒
                              0,51% OpRequest::_unregistered                       ▒
                   + 0,79% WeightedPriorityQueue<OpQueueItem, unsigned long>::deque▒
    ```
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/TrackedOp.cc
+++ src/common/TrackedOp.cc
@@ -274,7 +274,7 @@ bool OpTracker::register_inflight_op(TrackedOp *i)
     return false;
 
   uint64_t current_seq = ++seq;
-  uint32_t shard_index = current_seq % sharded_in_flight_list.size();
+  uint32_t shard_index = current_seq & (sharded_in_flight_list.size() - 1);
   ShardedTrackingData& sdata = sharded_in_flight_list[shard_index];
   {
     Mutex::Locker locker(sdata.ops_in_flight_lock_sharded);
@@ -289,7 +289,7 @@ void OpTracker::unregister_inflight_op(TrackedOp *i)
   // caller checks;
   assert(i->state);
 
-  uint32_t shard_index = i->seq % sharded_in_flight_list.size();
+  uint32_t shard_index = i->seq & (sharded_in_flight_list.size() - 1);
   ShardedTrackingData& sdata = sharded_in_flight_list[shard_index];
   {
     Mutex::Locker locker(sdata.ops_in_flight_lock_sharded);
