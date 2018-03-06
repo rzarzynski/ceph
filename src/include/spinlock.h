58faf76ffe14de750ac3015f515f8bc926270884
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-03-06 19:42:42 +0100

    fixup: optimize Ceph's spinlock with PAUSE.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/spinlock.h
+++ src/include/spinlock.h
@@ -20,12 +20,32 @@
 #include <atomic>
 #include <thread>
 
-#include <immintrin.h>
+#if defined(__i386__) || defined(__x86_64__)
+#  include <immintrin.h>
+#endif
 
 #include "common/likely.h"
 
 namespace ceph::spin {
 inline namespace version_1_0 {
+
+static inline void emit_pause() {
+#if defined(__i386__) || defined(__x86_64__)
+  // The spinning part is indistinguishable for CPU without
+  // additional hint. On x86 there is PAUSE instruction for
+  // that. We definitely want to use it because of:
+  //   * not disturbing second thread on the same core (SMT),
+  //   * saving power.
+  // Although the instruction is available since P4, binary
+  // transcription into `rep; nop` allows its decoding even
+  // on i386, so no need for `cpuid` or other costly things.
+  // For details please refer to:
+  //  * "Long Duration Spin-wait Loops on Hyper-Threading
+  //     Technology Enabled Intel Processors",
+  //  * "Benefitting Power and Performance Sleep Loops".
+  _mm_pause();
+#endif
+}
 
 
 template <class MutexT, std::size_t MaxTriesV = 64>
@@ -89,15 +109,32 @@ inline void spinlock::lock()
     return;
   }
 
+  // The contention part.
   do {
-    std::size_t r3 = 0;
+    std::size_t tries = 0;
+
+    // There is no need to constantly try LOCK CMPXCHG and enforce
+    // x86 CPUs to do costly mem fencing. The algorithm comes from
+    // NPTL's pthread_spin_lock() implementation of glibc.
     do {
-      asm("pause");
-      if (++r3 == 32) {
+      emit_pause();
+
+      if if (++tries == 32) {
+        // Oops, things went really bad. There was no state change
+        // for many iterations. This could happen when lock holder
+        // gets stuck because of e.g. being preempted. Most likely
+        // other waiters started spinning as well. The best we can
+        // do is to limit our losses and yield CPU. Luckily kernel
+        // (it's perfectly unaware about the whole situation) will
+        // switch to something useful or, at least, blocked holder
+        // will get its time quantum faster.
         std::this_thread::yield();
+        tries = 0;
       }
     } while (locked.load(std::memory_order_relaxed));
 
+    // The specification of compare_exchange() does not say a word
+    // that the value of `expected` can't change, so let's refresh.
     expected = false;
   } while (!locked.compare_exchange_weak(expected, true,
                                          std::memory_order_acquire,
