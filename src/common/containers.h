9e2513c5a29dbb254537feb5903362890b98eafa
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-03-20 02:06:11 +0100

    common: introduce ceph::containers::shard_vector.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/containers.h
+++ src/common/containers.h
@@ -16,6 +16,8 @@
 
 #include <type_traits>
 
+#include "include/intarith.h"
+
 namespace ceph::containers {
 
 // tiny_vector - a CPU-friendly container like small_vector but for
@@ -48,10 +50,12 @@ namespace ceph::containers {
 //  4. std::unique_ptr<ValueT>: extra indirection together with memory
 //     fragmentation.
 
-template<typename Value, std::size_t Capacity>
+template<typename Value,
+         std::size_t Capacity,
+         std::size_t Alignment = alignof(Value)>
 class tiny_vector {
   using storage_unit_t = \
-    std::aligned_storage_t<sizeof(Value), alignof(Value)>;
+    std::aligned_storage_t<sizeof(Value), Alignment>;
 
   std::size_t _size = 0;
   storage_unit_t* const data = nullptr;
@@ -137,6 +141,36 @@ public:
   }
   const pointer cend() const {
     return reinterpret_cast<pointer>(&data[_size]);
+  }
+};
+
+
+static constexpr const std::size_t CACHE_LINE_SIZE = 64;
+
+template<typename Value, std::size_t Capacity>
+class shard_vector : public tiny_vector<Value, Capacity, CACHE_LINE_SIZE> {
+  typedef tiny_vector<Value, Capacity, CACHE_LINE_SIZE> base_t;
+  typedef typename base_t::size_type size_type;
+  typedef typename base_t::reference reference;
+  typedef typename base_t::const_reference const_reference;
+
+public:
+  template<typename F>
+  shard_vector(const ceph::math::p2_t<size_type> count, F&& f)
+    : base_t(count, std::forward<F>(f)) {
+    assert(count == base_t::size());
+  }
+
+  ceph::math::p2_t<size_type> size() const {
+    return ceph::math::p2_t<size_type>::from_p2(base_t::size());
+  }
+
+  reference get_shard(const size_type selector) {
+    return (*this)[selector % size()];
+  }
+
+  const_reference get_shard(const size_type selector) const {
+    return (*this)[selector % size()];
   }
 };
 
