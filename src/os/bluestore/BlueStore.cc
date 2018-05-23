a313857a390042521d9b0467814242ca5543f072
Mohamad Gebai <mgebai@suse.com>
2018-05-23 11:40:43 -0400

    tracing: add tracepoints
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5177,14 +5177,17 @@ int BlueStore::_balance_bluefs_freespace(PExtentVector *extents)
       _dump_alloc_on_rebalance_failure();
       return 0;
     } else if (alloc_len < (int64_t)gift) {
-      dout(0) << __func__ << " insufficient allocate on 0x" << std::hex << gift
-              << " min_alloc_size 0x" << min_alloc_size 
-	      << " allocated 0x" << alloc_len
-	      << std::dec << dendl;
+//      dout(0) << __func__ << " insufficient allocate on 0x" << std::hex << gift
+//              << " min_alloc_size 0x" << min_alloc_size
+//	      << " allocated 0x" << alloc_len
+//	      << std::dec << dendl;
+      trace_bluefs_balance_freespace_insufficient_allocate(gift, min_alloc_size,
+        alloc_len);
       _dump_alloc_on_rebalance_failure();
     }
     for (auto& e : *extents) {
-      dout(1) << __func__ << " gifting " << e << " to bluefs" << dendl;
+//      dout(1) << __func__ << " gifting " << e << " to bluefs" << dendl;
+      trace_bluefs_balance_freespace_gifting_to_bluefs(e);
     }
     ret = 1;
   }
@@ -5314,8 +5317,9 @@ int BlueStore::_setup_block_symlink_or_file(
       string serial_number = epath.substr(strlen(SPDK_PREFIX));
       r = ::write(fd, serial_number.c_str(), serial_number.size());
       assert(r == (int)serial_number.size());
-      dout(1) << __func__ << " created " << name << " symlink to "
-              << epath << dendl;
+//      dout(1) << __func__ << " created " << name << " symlink to "
+//              << epath << dendl;
+      trace_bs_setup_block_symlink_created(name, epath);
       VOID_TEMP_FAILURE_RETRY(::close(fd));
     }
   }
@@ -5346,8 +5350,9 @@ int BlueStore::_setup_block_symlink_or_file(
 	    return -r;
 	  }
 	}
-	dout(1) << __func__ << " resized " << name << " file to "
-		<< byte_u_t(size) << dendl;
+//	dout(1) << __func__ << " resized " << name << " file to "
+//		<< byte_u_t(size) << dendl;
+        trace_bs_setup_block_symlink_resized(name, byte_u_t(size));
       }
       VOID_TEMP_FAILURE_RETRY(::close(fd));
     } else {
@@ -5364,7 +5369,8 @@ int BlueStore::_setup_block_symlink_or_file(
 
 int BlueStore::mkfs()
 {
-  dout(1) << __func__ << " path " << path << dendl;
+//  dout(1) << __func__ << " path " << path << dendl;
+  trace_bluestore_mkfs_create(path);
   int r;
   uuid_d old_fsid;
 
@@ -5372,7 +5378,8 @@ int BlueStore::mkfs()
     string done;
     r = read_meta("mkfs_done", &done);
     if (r == 0) {
-      dout(1) << __func__ << " already created" << dendl;
+//      dout(1) << __func__ << " already created" << dendl;
+      trace_bluestore_mkfs_create_already_created(path);
       if (cct->_conf->bluestore_fsck_on_mkfs) {
         r = fsck(cct->_conf->bluestore_fsck_on_mkfs_deep);
         if (r < 0) {
@@ -5422,9 +5429,11 @@ int BlueStore::mkfs()
   if (r < 0 || old_fsid.is_zero()) {
     if (fsid.is_zero()) {
       fsid.generate_random();
-      dout(1) << __func__ << " generated fsid " << fsid << dendl;
+//      dout(1) << __func__ << " generated fsid " << fsid << dendl;
+      trace_bluestore_mkfs_create_using_fsid(fsid, true);
     } else {
-      dout(1) << __func__ << " using provided fsid " << fsid << dendl;
+//      dout(1) << __func__ << " using provided fsid " << fsid << dendl;
+      trace_bluestore_mkfs_create_using_fsid(fsid, false);
     }
     // we'll write it later.
   } else {
@@ -5587,7 +5596,8 @@ void BlueStore::set_cache_shards(unsigned num)
 
 int BlueStore::_mount(bool kv_only, bool open_db)
 {
-  dout(1) << __func__ << " path " << path << dendl;
+//  dout(1) << __func__ << " path " << path << dendl;
+  trace_bluestore_mount_path(path);
 
   _kv_only = kv_only;
 
@@ -5701,7 +5711,8 @@ int BlueStore::_mount(bool kv_only, bool open_db)
 int BlueStore::umount()
 {
   assert(_kv_only || mounted);
-  dout(1) << __func__ << dendl;
+//  dout(1) << __func__ << dendl;
+  trace_bluestore_unmount(0);
 
   _osr_drain_all();
 
@@ -9488,7 +9499,7 @@ int BlueStore::queue_transactions(
   if (cct->_conf->objectstore_blackhole) {
 //    dout(0) << __func__ << " objectstore_blackhole = TRUE, dropping transaction"
 //	    << dendl;
-    trace_bluestore_queue_transaction_blackhole(0);
+    trace_bluestore_queue_transactions_blackhole(0);
     for (auto& l : { on_applied, on_commit, on_applied_sync }) {
       for (auto c : l) {
 	delete c;
