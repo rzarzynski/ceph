1929b51522c77c36cd31abcc064137e18ec67ee8
Mohamad Gebai <mgebai@suse.com>
2018-06-06 21:36:06 -0400

    test/fio: remove cout stuff for debugging
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/test/fio/fio_ceph_objectstore.cc
+++ src/test/fio/fio_ceph_objectstore.cc
@@ -329,11 +329,6 @@ Engine::Engine(thread_data* td)
                                g_conf->osd_objectstore,
                                g_conf->osd_data,
                                g_conf->osd_journal));
-  std::cout << std::endl;
-  std::cout << g_conf->osd_objectstore << std::endl;
-  std::cout << g_conf->osd_data << std::endl;
-  std::cout << g_conf->osd_journal<< std::endl;
-  std::cout << std::endl;
   if (!os)
     throw std::runtime_error("bad objectstore type " + g_conf->osd_objectstore);
 
