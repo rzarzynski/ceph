e702716c57e84503fe0c90ff91e2373c5dc4c98e
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-09-16 20:46:20 -0400

    fixup: fix and optimize slab_list for the *this == other case.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/slab_containers.h
+++ src/include/slab_containers.h
@@ -521,21 +521,30 @@ struct slab_list : public std::list<node,slab_allocator<pool_ix,node,stackCount,
    //
    // We support splice, but it requires actually copying each node, so it's O(N) not O(1)
    //
+   // TODO: static_asserts for original splice() + rename the stuff below to splice_same
    void splice(it pos, slab_list& other) {
-     this->splice(pos, other, other.begin(), other.end());
+     if (std::addressof(other) == this) {
+       base::splice(pos, other);
+       return;
+     } else {
+       ceph_assert("not supported" == nullptr);
+     }
    }
    void splice(it pos, slab_list& other, it it) {
-     this->splice(pos, other, it, it == other.end() ? it : std::next(it));
+     if (std::addressof(other) == this) {
+       base::splice(pos, other, it);
+       return;
+     } else {
+       ceph_assert("not supported" == nullptr);
+     }
    }
    void splice(it pos, slab_list& other, it first, it last) {
-      if (&other == this) {
-        base::splice(pos, other, first, last);
-        return;
-      }
-      while (first != last) {
-         pos = std::next(this->insert(pos,*first)); // points after insertion of this element
-         first = other.erase(first);
-      }
+     if (std::addressof(other) == this) {
+       base::splice(pos, other, first, last);
+       return;
+     } else {
+       ceph_assert("not supported" == nullptr);
+     }
    }
    //
    // Swap is supported, but it's O(2N)
