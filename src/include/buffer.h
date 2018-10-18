f3e7c2b7c8fae724d994fe959a95d2f384bcfd7d
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-10-18 22:33:12 +0200

    fixup for contgous_filler.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -749,7 +749,7 @@ namespace buffer CEPH_BUFFER_API {
       // TODO: consider making len a non-type template parameter.
       auto CEPH_INLINE append_hole(const unsigned len) {
         commit_length_update_n_invalidate();
-        return filler = bl.append_hole(len);
+        return bl.append_hole(len);
       }
 
       auto CEPH_INLINE length() const {
