d1af535647769d097b33cfa58dfb5c391f7e75db
Yingxin <yingxin.cheng@intel.com>
2018-12-28 10:51:25 +0800

    crimson/net: add proper gating and fault handling for send/keepalive
    Signed-off-by: Yingxin <yingxin.cheng@intel.com>

--- src/crimson/net/SocketConnection.h
+++ src/crimson/net/SocketConnection.h
@@ -160,6 +160,9 @@ class SocketConnection : public Connection {
 
   void execute_open();
 
+  seastar::future<> do_send(MessageRef msg);
+  seastar::future<> do_keepalive();
+
  public:
   SocketConnection(SocketMessenger& messenger,
                    Dispatcher& dispatcher);
