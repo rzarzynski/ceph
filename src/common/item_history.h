5623562a26b6ae8478674351b650dfdf7d627ed7
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-01-16 13:21:25 +0100

    common: drop the unnused prune() of safe_item_history.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/item_history.h
+++ src/common/item_history.h
@@ -93,11 +93,4 @@ public:
     return *current;
   }
 
-  void prune() {
-    // note: this is not necessarily thread-safe wrt readers
-    std::lock_guard l(lock);
-    while (history.size() > 1) {
-      history.pop_front();
-    }
-  }
 };
