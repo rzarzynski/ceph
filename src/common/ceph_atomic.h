910760a32ef513fb8bb9818bd58f12d026a1d17c
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-01-21 13:08:13 +0100

    fixup: introduce ceph::atomic and apply it on bufferlist.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/ceph_atomic.h
+++ src/common/ceph_atomic.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <atomic>
+
 // What and why
 // ============
 //
@@ -17,12 +19,64 @@
 
 #ifdef WITH_SEASTAR
 
+#include <type_traits>
+
 namespace ceph {
-  template <class T> using atomic = T;
+  template <class T>
+  class dummy_atomic {
+    T value;
+
+  public:
+    dummy_atomic() = default;
+    dummy_atomic(const dummy_atomic&) = delete;
+    dummy_atomic(T value) : value(std::move(value)) {
+    }
+    bool is_lock_free() const noexcept {
+      return true;
+    }
+    void store(T desired, std::memory_order) noexcept {
+      value = std::move(desired);
+    }
+    T load(std::memory_order = std::memory_order_seq_cst) const noexcept {
+      return value;
+    }
+    T operator=(T desired) noexcept {
+      value = std::move(desired);
+    }
+    operator T() const noexcept {
+      return value;
+    }
+
+    // We need to differentiate with SFINAE as std::atomic offers beefier
+    // interface for integral types.
+    std::enable_if_t<std::is_integral_v<T>, T> operator++() {
+      return ++value;
+    }
+    std::enable_if_t<std::is_integral_v<T>, T> operator++(int) {
+      return value++;
+    }
+    std::enable_if_t<std::is_integral_v<T>, T> operator--() {
+      return --value;
+    }
+    std::enable_if_t<std::is_integral_v<T>, T> operator--(int) {
+      return value--;
+    }
+    std::enable_if_t<std::is_integral_v<T>, T> operator+=(const dummy_atomic& b) {
+      value += b;
+      return value;
+    }
+    std::enable_if_t<std::is_integral_v<T>, T> operator-=(const dummy_atomic& b) {
+      value -= b;
+      return value;
+    }
+
+    static constexpr bool is_always_lock_free = true;
+  };
+
+  template <class T> using atomic = dummy_atomic<T>;
 } // namespace ceph
 
 #else  // WITH_SEASTAR
-#  include <atomic>
 
 namespace ceph {
   template <class T> using atomic = ::std::atomic<T>;
