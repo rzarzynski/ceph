eda0d4f3cc704bb1f099062ecdb373880e6d1335
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-03-07 19:51:34 +0100

    fixup
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/test/crypto_onwire.cc
+++ src/test/crypto_onwire.cc
@@ -23,14 +23,41 @@ public:
   }
 };
 
+struct aes_gcm_sample_t {
+  static constexpr std::string_view key_and_nonce {
+    "mock of crypto material for deriving key and nonce for AES GCM"
+  };
+
+  static constexpr std::string_view plaintext {
+    "mock of plain text for AES GCM cipher"
+  };
+
+  // AES GCM doesn't do padding. The size of ciphertext is actually the same
+  // as its corresponding plaintext except the AE (authenticated encryption)
+  // tag at the end.
+  static constexpr std::array<unsigned char,
+                              std::size(plaintext)> ciphertext {
+    0x4d, 0xa2, 0xa6, 0x1b, 0xa5, 0x2e, 0x20, 0x0d,
+    0xa3, 0x3e, 0x56, 0x6f, 0x36, 0x8c, 0xf3, 0x43,
+    0x1a, 0xe5, 0x81, 0x55, 0xb2, 0x31, 0x8c, 0x79,
+    0xe5, 0x16, 0xae, 0xab, 0x80, 0xab, 0xd9, 0xe4,
+    0x13, 0x91, 0xad, 0x44, 0x7d
+  };
+
+  static constexpr std::array<unsigned char,
+                              ceph::crypto::onwire::AESGCM_TAG_LEN> tag {
+    0xf4, 0x91, 0x9e, 0x37, 0x0e, 0xdc, 0xa8, 0xb2,
+    0xc6, 0xeb, 0xf8, 0x03, 0xe9, 0x62, 0x42, 0xc5
+  };
+};
+
 template <class T>
 static std::unique_ptr<T> create_crypto_handler(CephContext* const cct)
 {
   using ceph::crypto::onwire::key_t;
   using ceph::crypto::onwire::nonce_t;
 
-  const auto connection_secret = \
-    "mock of crypto material for deriving key and nonce for AES GCM"sv;
+  const auto& connection_secret = aes_gcm_sample_t::key_and_nonce;
   ceph_assert_always(
     connection_secret.length() >= sizeof(key_t) + sizeof(nonce_t));
   const char* secbuf = connection_secret.data();
@@ -49,69 +76,6 @@ static std::unique_ptr<T> create_crypto_handler(CephContext* const cct)
 
   return std::make_unique<T>(cct, key, nonce);
 }
-
-static auto constexpr pt = "mock of plain text for AES GCM cipher"sv;
-
-TEST(OnwireCrypto, AESGCMTxHandler)
-{
-  using ceph::crypto::onwire::AES128GCM_OnWireTxHandler;
-  auto tx = create_crypto_handler<AES128GCM_OnWireTxHandler>(g_ceph_context);
-
-  ceph::bufferlist plaintext;
-  {
-    auto pt = "mock of plain text for AES GCM cipher"sv;
-    plaintext.push_back(ceph::buffer::copy(pt.data(), pt.length()));
-  }
-
-  tx->reset_tx_handler({ plaintext.length() });
-  tx->authenticated_encrypt_update(plaintext);
-  auto ciphertext = tx->authenticated_encrypt_final();
-  using ceph::crypto::onwire::AESGCM_TAG_LEN;
-  ASSERT_EQ(pt.size() + AESGCM_TAG_LEN, ciphertext.length());
-  ASSERT_NE(0, ::memcmp(ciphertext.c_str(), pt.data(), pt.size()));
-
-  // let's ensure the input bufferlist is untoched.
-  ASSERT_TRUE(plaintext.contents_equal(pt.data(), pt.size()));
-}
-
-// FIXME: replace with recording
-auto create_ciphertext() {
-  using ceph::crypto::onwire::AES128GCM_OnWireTxHandler;
-  auto tx = create_crypto_handler<AES128GCM_OnWireTxHandler>(g_ceph_context);
-
-  ceph::bufferlist plaintext;
-  {
-    plaintext.push_back(ceph::buffer::copy(pt.data(), pt.length()));
-  }
-
-  tx->reset_tx_handler({ plaintext.length() });
-  tx->authenticated_encrypt_update(plaintext);
-  return tx->authenticated_encrypt_final();
-}
-
-struct aes_gcm_sample_t {
-  static constexpr std::string_view plain {
-    "mock of plain text for AES GCM cipher"
-  };
-
-  // AES GCM doesn't do padding. The size of ciphertext is actually the same
-  // as its corresponding plaintext except the AE (authenticated encryption)
-  // tag at the end.
-  static constexpr std::array<unsigned char,
-                              std::size(plain)> encrypted {
-    0x4d, 0xa2, 0xa6, 0x1b, 0xa5, 0x2e, 0x20, 0x0d,
-    0xa3, 0x3e, 0x56, 0x6f, 0x36, 0x8c, 0xf3, 0x43,
-    0x1a, 0xe5, 0x81, 0x55, 0xb2, 0x31, 0x8c, 0x79,
-    0xe5, 0x16, 0xae, 0xab, 0x80, 0xab, 0xd9, 0xe4,
-    0x13, 0x91, 0xad, 0x44, 0x7d
-  };
-
-  static constexpr std::array<unsigned char,
-                              ceph::crypto::onwire::AESGCM_TAG_LEN> tag {
-    0xf4, 0x91, 0x9e, 0x37, 0x0e, 0xdc, 0xa8, 0xb2,
-    0xc6, 0xeb, 0xf8, 0x03, 0xe9, 0x62, 0x42, 0xc5
-  };
-};
 
 template <std::size_t N>
 static ceph::bufferlist to_bl(const std::array<unsigned char, N>& arr)
@@ -142,12 +106,37 @@ struct ciphertext_generator_t {
   }
 
   ceph::bufferlist generate_cipherchunk_with_tag() {
-    auto plainchunk = to_bl(aes_gcm_sample_t::plain);
+    auto plainchunk = to_bl(aes_gcm_sample_t::plaintext);
     tx->reset_tx_handler({ plainchunk.length() });
     tx->authenticated_encrypt_update(std::move(plainchunk));
     return tx->authenticated_encrypt_final();
   }
 };
+
+TEST(AESGCMTxHandler, encrypt_sample)
+{
+  auto tx = create_crypto_handler<AES128GCM_OnWireTxHandler>(g_ceph_context);
+  auto plaintext = to_bl(aes_gcm_sample_t::plaintext);
+
+  tx->reset_tx_handler({ plaintext.length() });
+  tx->authenticated_encrypt_update(plaintext);
+  auto ciphertext_with_tag = tx->authenticated_encrypt_final();
+
+  using ceph::crypto::onwire::AESGCM_TAG_LEN;
+  const auto plaintext_size = aes_gcm_sample_t::plaintext.size();
+  ASSERT_EQ(plaintext_size + AESGCM_TAG_LEN,
+            ciphertext_with_tag.length());
+  ASSERT_EQ(0, ::memcmp(ciphertext_with_tag.c_str(),
+                        aes_gcm_sample_t::ciphertext.data(),
+                        aes_gcm_sample_t::ciphertext.size()));
+  ASSERT_EQ(0, ::memcmp(ciphertext_with_tag.c_str() + plaintext_size,
+                        aes_gcm_sample_t::tag.data(),
+                        aes_gcm_sample_t::tag.size()));
+
+  // let's ensure the input bufferlist is untoched.
+  ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plaintext.data(),
+                                       aes_gcm_sample_t::plaintext.size()));
+}
 
 
 TEST(AESGCMRxHandler, single_chunk)
@@ -159,7 +148,7 @@ TEST(AESGCMRxHandler, single_chunk)
   ceph::bufferlist ciphertext_with_tag;
   {
     // claim_append() needs l-value reference.
-    auto ciphertext = to_bl(aes_gcm_sample_t::encrypted);
+    auto ciphertext = to_bl(aes_gcm_sample_t::ciphertext);
     auto tag = to_bl(aes_gcm_sample_t::tag);
     ciphertext_with_tag.claim_append(ciphertext);
     ciphertext_with_tag.claim_append(tag);
@@ -168,8 +157,8 @@ TEST(AESGCMRxHandler, single_chunk)
   // If tag doesn't match, exception will be thrown.
   auto plaintext = rx->authenticated_decrypt_update_final(
     std::move(ciphertext_with_tag), 16);
-  ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plain.data(),
-                                       aes_gcm_sample_t::plain.size()));
+  ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plaintext.data(),
+                                       aes_gcm_sample_t::plaintext.size()));
 }
 
 TEST(AESGCMRxHandler, mismatched_tag)
@@ -180,7 +169,7 @@ TEST(AESGCMRxHandler, mismatched_tag)
   ceph::bufferlist ciphertext_with_badtag;
   {
     // claim_append() needs l-value reference.
-    auto ciphertext = to_bl(aes_gcm_sample_t::encrypted);
+    auto ciphertext = to_bl(aes_gcm_sample_t::ciphertext);
     ceph::bufferlist tag;
     tag.append_zero(ceph::crypto::onwire::AESGCM_TAG_LEN);
     ciphertext_with_badtag.claim_append(ciphertext);
@@ -201,14 +190,14 @@ TEST(AESGCMRxHandler, multi_chunk)
   // to authenticated_decrypt_update() – the auth tag in this test is
   // provided separately.
   for (std::size_t chunk_size = 1;
-       chunk_size <= std::size(aes_gcm_sample_t::encrypted);
+       chunk_size <= std::size(aes_gcm_sample_t::ciphertext);
        chunk_size++) {
     auto rx = create_crypto_handler<AES128GCM_OnWireRxHandler>(g_ceph_context);
 
     rx->reset_rx_handler();
 
     ceph::bufferlist plaintext;
-    ceph::bufferlist ciphertext = to_bl(aes_gcm_sample_t::encrypted);
+    ceph::bufferlist ciphertext = to_bl(aes_gcm_sample_t::ciphertext);
     while (ciphertext.length() >= chunk_size) {
       ceph::bufferlist cipherchunk;
       ciphertext.splice(0, chunk_size, &cipherchunk);
@@ -228,8 +217,8 @@ TEST(AESGCMRxHandler, multi_chunk)
     auto final_plainchunk = \
       rx->authenticated_decrypt_update_final(to_bl(aes_gcm_sample_t::tag), 16);
     ASSERT_EQ(0, final_plainchunk.length());
-    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plain.data(),
-                                         aes_gcm_sample_t::plain.size()));
+    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plaintext.data(),
+                                         aes_gcm_sample_t::plaintext.size()));
   }
 }
 
@@ -249,8 +238,8 @@ TEST(AESGCMRxHandler, reset)
       plaintext = rx->authenticated_decrypt_update_final(
         std::move(ciphertext_with_tag), 16);
     });
-    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plain.data(),
-                                         aes_gcm_sample_t::plain.size()));
+    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plaintext.data(),
+                                         aes_gcm_sample_t::plaintext.size()));
   }
 }
 
@@ -279,8 +268,8 @@ TEST(AESGCMRxHandler, reset_with_multiple_chunks)
       plaintext = rx->authenticated_decrypt_update(
         std::move(ciphertext), 16);
     });
-    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plain.data(),
-                                         aes_gcm_sample_t::plain.size()));
+    ASSERT_TRUE(plaintext.contents_equal(aes_gcm_sample_t::plaintext.data(),
+                                         aes_gcm_sample_t::plaintext.size()));
 
     // If tag doesn't match, exception will be thrown.
     ceph::bufferlist final_plaintext;
