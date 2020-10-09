52612f91e7cef6adb859c73275707a9987c6f598
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-10-09 17:24:16 +0200

    rgw: turn RGWSI_BILog_RADOS_InIndex into an interface.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/rgw/services/svc_bilog_rados.h
+++ src/rgw/services/svc_bilog_rados.h
@@ -27,30 +27,55 @@
 class RGWSI_BILog_RADOS : public RGWServiceInstance
 {
 public:
+  RGWSI_BILog_RADOS(CephContext *cct);
+
+  virtual void init(RGWSI_BucketIndex_RADOS *bi_rados_svc);
+
+  virtual int log_start(const RGWBucketInfo& bucket_info, int shard_id);
+  virtual int log_stop(const RGWBucketInfo& bucket_info, int shard_id);
+
+  virtual int log_trim(const RGWBucketInfo& bucket_info,
+                       int shard_id,
+                       std::string& start_marker,
+                       std::string& end_marker);
+  virtual int log_list(const RGWBucketInfo& bucket_info,
+                       int shard_id,
+                       std::string& marker,
+                       uint32_t max,
+                       std::list<rgw_bi_log_entry>& result,
+                       bool *truncated);
+
+  virtual int get_log_status(const RGWBucketInfo& bucket_info,
+                             int shard_id,
+                             map<int, string> *markers);
+};
+
+class RGWSI_BILog_RADOS_InIndex : public RGWSI_BILog_RADOS
+{
+public:
   struct Svc {
     RGWSI_BucketIndex_RADOS *bi{nullptr};
   } svc;
 
-  RGWSI_BILog_RADOS(CephContext *cct);
+  using RGWSI_BILog_RADOS::RGWSI_BILog_RADOS;
 
-  void init(RGWSI_BucketIndex_RADOS *bi_rados_svc);
+  void init(RGWSI_BucketIndex_RADOS *bi_rados_svc) override;
 
-  int log_start(const RGWBucketInfo& bucket_info, int shard_id);
-  int log_stop(const RGWBucketInfo& bucket_info, int shard_id);
+  int log_start(const RGWBucketInfo& bucket_info, int shard_id) override;
+  int log_stop(const RGWBucketInfo& bucket_info, int shard_id) override;
 
   int log_trim(const RGWBucketInfo& bucket_info,
                int shard_id,
                std::string& start_marker,
-               std::string& end_marker);
+               std::string& end_marker) override;
   int log_list(const RGWBucketInfo& bucket_info,
                int shard_id,
                std::string& marker,
                uint32_t max,
                std::list<rgw_bi_log_entry>& result,
-               bool *truncated);
+               bool *truncated) override;
 
   int get_log_status(const RGWBucketInfo& bucket_info,
                      int shard_id,
-                     map<int, string> *markers);
+                     map<int, string> *markers) override;
 };
-
