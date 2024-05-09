ebf65f7b69edad95360a794d74c3dbb17f6d5527
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-05-09 19:07:32 +0000

    osd: abstract sending MOSDPGPush during EC recovery
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/osd/ECCommon.h
+++ src/osd/ECCommon.h
@@ -21,6 +21,9 @@
 #include "erasure-code/ErasureCodeInterface.h"
 #include "ECUtil.h"
 #include "ECTypes.h"
+#include "messages/MOSDPGPush.h"
+#include "messages/MOSDPGPushReply.h"
+#include "msg/MessageRef.h"
 #if WITH_CRIMSON
 #include "ExtentCache.h"
 #include "crimson/osd/object_context.h"
--- src/osd/ECListener.h
+++ src/osd/ECListener.h
@@ -144,6 +144,8 @@ struct ECListener {
   // XXX
   virtual void send_message_osd_cluster(
     std::vector<std::pair<int, Message*>>& messages, epoch_t from_epoch) = 0;
+  virtual void send_message_osd_cluster(
+    int osd, MOSDPGPush* msg, epoch_t from_epoch) = 0;
 
   virtual std::ostream& gen_dbg_prefix(std::ostream& out) const = 0;
 
