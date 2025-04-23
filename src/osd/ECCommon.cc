5bdc7a5ee10333758de8fcf428f64b807e5ad3e3
Radoslaw Zarzynski <rzarzyns@redhat.com>
2025-04-23 10:10:21 +0000

    fixup: osd: ECCommon respects crimson non-null obc for new objects
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/osd/ECCommon.cc
+++ src/osd/ECCommon.cc
@@ -1490,7 +1490,7 @@ void ECCommon::RecoveryBackend::continue_recovery_op(
 	amount,
 	std::move(want),
 	to_read,
-#ifdef WITH_SEASTAR
+#ifdef WITH_CRIMSON
 	op.recovery_progress.first && op.xattrs.count(OI_ATTR) == 0);
 #else
 	op.recovery_progress.first && !op.obc);
